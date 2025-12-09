036a2afabe492c262b6a5468f80adc62d02845ed
strangecodee <anuragmaurya883@gmail.com>
2025-12-09 12:04:46 +0530

    DB shift to Psql

--- models/User.js
+++ models/User.js
@@ -1,20 +1,30 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../sequelize.js";
 
-const User = sequelize.define("User", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: { isEmail: true },
-    set(value) { this.setDataValue("email", value.trim().toLowerCase()); },
+const User = sequelize.define(
+  "User",
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true },
+      set(value) {
+        this.setDataValue("email", value.trim().toLowerCase());
+      },
+    },
+    deletedAt: {
+      // <-- REQUIRED FOR POSTGRES WHEN paranoid:true
+      type: DataTypes.DATE,
+    },
   },
-}, {
-  timestamps: true,
-  paranoid: true,
-  tableName: "users",
-});
+  {
+    timestamps: true,
+    paranoid: true,
+    tableName: "users",
+  }
+);
 
 export default User;
